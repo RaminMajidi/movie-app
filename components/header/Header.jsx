c2a30528ab21362ef1aad22f54b46ffc0286ee1a
RaminMajidi <raminmajididev01@gmail.com>
2023-06-25 21:44:37 +0330

    Change Style

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <header className="bg-[var(--c-bg)] text-[var(--c-gray)]
          border-b-2 font-bold  fixed w-full h-[3rem] top-0 left-0
-          z-50 px-2 py-4 flex justify-between items-center">
+          z-50 px-6 py-4 flex justify-between items-center">
             <div>
-                <h2 className="hidden sm:block">Movie App</h2>
+                <h2 className="">Movie App</h2>
             </div>
 
             <div className="flex">
@@ -26,28 +26,28 @@ const Header = () => {
                         <span>Movies</span>
                     </button>
                     <ul className="group rounded-lg absolute hidden
-                     group-hover:block  text-center -left-10 top-6 
-                     w-36 bg-[var(--c-orange)] text-[var(--c-black)]">
+                     group-hover:block  text-center -left-6 top-6 
+                     w-28 bg-[var(--c-orange)] text-[var(--c-black)]">
 
-                        <Link href={`/movie/upcoming`}>
+                        <Link href={'/movie/upcoming'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer rounded-t-lg">Upcoming</li>
                         </Link>
 
-                        <Link href={`/movie/nowPlaying`}>
+                        <Link href={'/movie/nowPlaying'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer">Now Playing</li>
                         </Link>
 
-                        <Link href={`/movie/popular`}>
+                        <Link href={'/movie/popular'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer">Popular</li>
                         </Link>
 
-                        <Link href={`/movie/topRated`}>
+                        <Link href={'/movie/topRated'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer rounded-b-lg">Top Rated</li>
@@ -62,39 +62,33 @@ const Header = () => {
                         <span>Tv's</span>
                     </button>
                     <ul className="group rounded-lg absolute hidden
-                     group-hover:block  text-center -left-10 top-6 
-                     w-36 bg-[var(--c-orange)] text-[var(--c-black)]">
+                     group-hover:block  text-center -left-16 top-6 
+                     w-28 bg-[var(--c-orange)] text-[var(--c-black)]">
 
-                        <Link href={`/tv/airingToday`}>
+                        <Link href={'/tv/airingToday'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer rounded-t-lg">Airing Today</li>
                         </Link>
 
-                        <Link href={`/tv/onTheAir`}>
+                        <Link href={'/tv/onTheAir'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer">On The Air</li>
                         </Link>
 
-                        <Link href={`/tv/popular`}>
+                        <Link href={'/tv/popular'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer">Popular</li>
                         </Link>
 
-                        <Link href={`/tv/topRated`}>
+                        <Link href={'/tv/topRated'}>
                             <li className="p-1 hover:bg-[var(--c-black)]
                          hover:text-[var(--c-orange)] transition-all
                           cursor-pointer rounded-b-lg">Top Rated</li>
                         </Link>
                     </ul>
-                </div>
-
-                <div>
-                    <Link href={'/myList'} className="mx-2 hover:text-[var(--c-orange)]  transition-all duration-700">
-                        <span>MyList</span>
-                    </Link>
                 </div>
             </div>
         </header>
