1715e2709120d38adfa8b9b82061eadfee34b4b6
RaminMajidi <raminmajididev01@gmail.com>
2023-06-30 17:50:44 +0330

    Minor changes In Home Page

--- app/page.js
+++ app/page.js
@@ -1,31 +1,30 @@
 import HomeSlider from "@/components/homeSlider/HomeSlider"
 export default async function Home() {
 
-  const getDat = async () =>{
-    let result = [];
-    const res = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',{
+  const getDat = async () => {
+    const res = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', {
       method: 'GET',
       headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${process.env.ACCIC_TOKEN}`
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.ACCIC_TOKEN}`
       }
     });
-    if(res.status === 200){
-      result =  await res.json()
+    if (res.status === 200) {
+      const result = await res.json()
       return result;
     }
-    else{
-      return result;
+    else {
+      return false;
     }
-     
+
   }
   const data = await getDat();
 
   return (
     <>
-    {data &&
-    <HomeSlider key={"test"} data={data}/>
-    }
+      {data &&
+        <HomeSlider key={"test"} data={data} />
+      }
     </>
   )
 }
