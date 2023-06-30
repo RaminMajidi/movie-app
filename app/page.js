a83e4aca49caae1f0b6203f4bbfc8ea7fbc406dd
RaminMajidi <raminmajididev01@gmail.com>
2023-06-30 20:26:20 +0330

    Minor changes

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import HomeSlider from "@/components/homeSlider/HomeSlider"
+
 export default async function Home() {
   const res = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', {
     method: 'GET',
--- components/homeSlider/HomeSlider.jsx
+++ components/homeSlider/HomeSlider.jsx
@@ -16,67 +16,72 @@ import { Parallax, Pagination, Navigation } from "swiper";
 const HomeSlider = async ({ data }) => {
 
     const router = useRouter();
-    const [sliderData, setSliderData] = useState([])
+    const [sliderData, setSliderData] = useState(data)
 
     useEffect(() => {
-        console.log(data)
         setSliderData(data)
-    }, [])
+        console.log(data)
+    }, [data])
 
     return (
         <>
-            <Swiper
-                style={{
-                    "--swiper-navigation-color": "#fff",
-                    "--swiper-pagination-color": "#fff",
-                }}
-                speed={1000}
-                parallax={true}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                modules={[Parallax, Pagination, Navigation]}
-                className="mySwiper homeSwiper"
-            >
-                {
-                    sliderData.map((item) => (
-                        <SwiperSlide key={item.id} className="relative rounded-xl" >
-                            <ImgLoader
-                                alt={item.title}
-                                src={item.backdrop_path}
-                                style={'rounded-xl'} />
-                            <section className="absolute top-1 pl-12 w-full ">
-                                <div className="title text-[var(--c-orange)]"
-                                    data-swiper-parallax="-800">
-                                    {item.title}
-                                </div>
-                                <div className="subtitle" data-swiper-parallax="-600">
-                                    <AverageStars
-                                        col={false}
-                                        average={item.vote_average} />
-                                </div>
-                                <div className="text" data-swiper-parallax="-500">
-                                    <p className="text-justify bg-[rgba(0,0,0,0.5)] text-base mt-2
-                                             p-2 rounded-md">
-                                        {item.overview}
-                                    </p>
-                                    <button
-                                        onClick={() => router.push(`/movie/${item.id}`)}
-                                        title={item.title}
-                                        className="bg-[var(--c-lblue)] px-4 py-2
-                                                 mt-4 rounded-md hover:bg-[var(--c-blue)]
-                                                  transition-all"
-                                        type="button"
-                                    >
-                                        Reade More
-                                    </button>
-                                </div>
-                            </section>
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+            {sliderData ? (
+                <Swiper
+                    style={{
+                        "--swiper-navigation-color": "#fff",
+                        "--swiper-pagination-color": "#fff",
+                    }}
+                    speed={1000}
+                    parallax={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Parallax, Pagination, Navigation]}
+                    className="mySwiper homeSwiper"
+                >
+                    {
+                        sliderData.map((item) => (
+                            <SwiperSlide key={item.id} className="relative rounded-xl" >
+                                <ImgLoader
+                                    alt={item.title}
+                                    src={item.backdrop_path}
+                                    style={'rounded-xl'} />
+                                <section className="absolute top-1 pl-12 w-full ">
+                                    <div className="title text-[var(--c-orange)]"
+                                        data-swiper-parallax="-800">
+                                        {item.title}
+                                    </div>
+                                    <div className="subtitle" data-swiper-parallax="-600">
+                                        <AverageStars
+                                            col={false}
+                                            average={item.vote_average} />
+                                    </div>
+                                    <div className="text" data-swiper-parallax="-500">
+                                        <p className="text-justify bg-[rgba(0,0,0,0.5)] text-base mt-2
+                               p-2 rounded-md">
+                                            {item.overview}
+                                        </p>
+                                        <button
+                                            onClick={() => router.push(`/movie/${item.id}`)}
+                                            title={item.title}
+                                            className="bg-[var(--c-lblue)] px-4 py-2
+                                   mt-4 rounded-md hover:bg-[var(--c-blue)]
+                                    transition-all"
+                                            type="button"
+                                        >
+                                            Reade More
+                                        </button>
+                                    </div>
+                                </section>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+
+            ) :
+                (<p>Loading...</p>)}
+
         </>
     )
 }
