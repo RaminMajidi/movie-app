4ae2ffdd11c3e4de034323000e7e1eb7111e4c99
RaminMajidi <raminmajididev01@gmail.com>
2023-07-01 13:45:29 +0330

    changes For Error Btn Back

--- components/btnBack/BtnBack.jsx
+++ components/btnBack/BtnBack.jsx
@@ -1,13 +1,12 @@
-'use client'
+"use client"
 import { useRouter } from "next/navigation";
-
 
 const BtnBack = ({ style }) => {
     const router = useRouter()
     return (
         <button
             className={`${style}`}
-            onClick={() => router.back()}
+            onClick={()=>router.back()}
         >
             Go back
         </button>
